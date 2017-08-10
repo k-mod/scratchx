d394bf3dd7fe9e324fd48d6546d0d5f8e53f9c19
k-mod <30876399+k-mod@users.noreply.github.com>
2017-08-10 09:49:57 +0100

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,4 +1,19 @@
 (function(ext) {
+    ext.pi_url = 'http://192.168.1.74:8000';
+    ext.pi_userpass = 'webiopi:raspberry';
+    ext.empty_post = function(url) {
+	      $.ajax({
+              type:'POST',
+              url: url,
+              data: { },
+              headers: {
+	    	    'Authorization': 'Basic ' + btoa(ext.pi_userpass)
+	          },
+	          error: function(err) { 
+	    	    alert (err); 
+	          }
+        });
+    };
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -9,29 +24,22 @@
     };
 
     ext.set_lcd = function(t) {
-	   
-        $.ajax({
-              type:'POST',
-              url: 'http://192.168.1.74:8000/macros/lcd_display/' + t,
-              data: { },
-              headers: {
-	    	    'Authorization': 'Basic ' + btoa('webiopi:raspberry')
-	          },
-	          error: function(err) { 
-	    	    alert (err); 
-	          }
-        });
-	
+	    ext.empty_post(ext.pi_url + '/macros/lcd_display/' + t);
     };
+    
+    
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
             [' ', 'Display %s on LCD', 'set_lcd'],
+	    [' ', 'Stop', 'stop'],
+		[' ', 'Move Forward', 'move_forward'],
+		[' ', 'Move Backward', 'move_backward'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
+    ScratchExtensions.register('My Bot Extensions', descriptor, ext);
 })({});
